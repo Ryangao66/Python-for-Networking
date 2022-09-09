57dc4d9c5f9dfa12cdbbdfd9bda08bbe1180a559
ryangao <ryangao66@gmail.com>
2022-09-10 00:26:14 +1200

    Daily practice for CCIE

--- Daily practice.py
+++ Daily practice.py
@@ -1,4 +1,10 @@
-'''查看系统保留字段，不可以用于定义变量'''
+'''查看系统保留字段
+
+不可以用于定义变量'''
+
+# 注释的方法有两种 # 为单行，'''为多行
+
+
 import keyword
 keyword.kwlist
 
@@ -8,8 +14,7 @@ print(dota)
 x100 = "Fujifilm x110v"
 print(x100)
 
-
-
-
+xt3 = "Fujicilm X-t3"
+print(xt3)
 
 
